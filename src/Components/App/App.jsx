3904821932ed108f793b5484032dfed692f10d64
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-09-14 20:50:35 -0600

    Restructure Components

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from '../Navbar/Navbar'
+
+const App = () => {
+
+
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
